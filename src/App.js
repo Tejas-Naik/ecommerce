cd8617e48982e8d9fea979c23e66604866b752c3
Tejas-Naik <RNTejas2005@gmail.com>
2023-02-03 05:42:46 +0000

    styled the navigation and made it look in al pages by routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => (
+  <h1>Shop page</h1>
+);
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
