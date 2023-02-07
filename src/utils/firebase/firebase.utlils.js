e43b9cd57ecc2ebbc95e368917b652f22b6a24c4
Tejas-Naik <RNTejas2005@gmail.com>
2023-02-07 13:55:30 +0000

    Configuring Firebase

---
+++ src/utils/firebase/firebase.utlils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// App config (registering the app for web on firebase)
+const firebaseConfig = {
+    apiKey: "AIzaSyCmQgWC3ZYE0czTM1H8TtKUvQQQxP05-Jk",
+    authDomain: "crwn-clothing-db-94dfc.firebaseapp.com",
+    projectId: "crwn-clothing-db-94dfc",
+    storageBucket: "crwn-clothing-db-94dfc.appspot.com",
+    messagingSenderId: "688591319593",
+    appId: "1:688591319593:web:58b8a2b943c3f25b19e238"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
